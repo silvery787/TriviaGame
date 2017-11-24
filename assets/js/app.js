fb3b1d101cd73ddad8c71b104f7990024adf1dfb
Victoria <victoria@some-mail.com>
2017-11-24 01:07:57 -0800

    added 'Astro'-theme (questions and styles)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,261 @@
 
-const MaxQuestionsToAsk = 3;
-const MaxTimer = 10;//seconds to answer
-const DelayTime = 2000;
+const MaxQuestionsToAsk = 10;
+const MaxTimer = 30;//seconds to answer
+const DelayTime = 3000;
 
 var questionList = [
 	{
-		q: "aaa", 
-		a1: "a", 
-		a2: "b", 
-		a3: "c", 
-		a4: "d", 
+		q: "Which is the second planet from the Sun?", 
+		a1: "Venus", 
+		a2: "Mars", 
+		a3: "Earth", 
+		a4: "Mercury", 
 		corr: 1, 
-		img : "image.gif"
+		img : "venus.jpg"
 	},
 	{
-		q: "bbb", 
-		a1: "a", 
-		a2: "b", 
-		a3: "c", 
-		a4: "d", 
-		corr: 2, 
-		img : "image.gif"
-	},
-	{
-		q: "ccc", 
-		a1: "a", 
-		a2: "b", 
-		a3: "c", 
-		a4: "d", 
+		q: "Phobos and Deimos are the moons of which planet in our solar system?", 
+		a1: "Venus", 
+		a2: "Jupiter", 
+		a3: "Mars", 
+		a4: "Uranus", 
 		corr: 3, 
-		img : "image.gif"
+		img : "mars-phobos_deimos.jpg"
 	},
-	{ q: "ddd", a1: "d", a2: "e", a3: "f", a4: "a", corr: 1, img : "image.gif"},
-	{ q: "eee", a1: "d", a2: "e", a3: "f", a4: "a", corr: 2, img : "image.gif"},
-	{ q: "fff", a1: "d", a2: "e", a3: "f", a4: "d", corr: 3, img : "image.gif"},	
+	{
+		q: "Titania, Miranda and Bianca are all moons of which planet in our solar system?", 
+		a1: "Jupiter", 
+		a2: "Uranus", 
+		a3: "Saturn", 
+		a4: "Mars", 
+		corr: 2, 
+		img : "uranus.jpg"
+	},
+	{ 
+		q: "What is the fifth planet from the Sun?", 
+		a1: "Jupiter", 
+		a2: "Venus", 
+		a3: "Saturn", 
+		a4: "Earth", 
+		corr: 1, 
+		img : "jupiter.jpg"
+	},
+	{ 
+		q: "How many planets are in our solar system have rings?", 
+		a1: "1", 
+		a2: "4", 
+		a3: "2", 
+		a4: "5", 
+		corr: 2, 
+		img : "planets.jpg"
+	},
+	{ 
+		q: "Which planet lies between Jupiter and Uranus?", 
+		a1: "Mars", 
+		a2: "Neptun", 
+		a3: "Saturn", 
+		a4: "Mercury", 
+		corr: 3, 
+		img : "saturn.jpg"
+	},
+	{ 
+		q: "Which is the fastest rotating planet in our solar system?", 
+		a1: "Mars", 
+		a2: "Jupiter", 
+		a3: "Earth", 
+		a4: "Mercury", 
+		corr: 2, 
+		img : "jupiter.jpg"
+	},
+	{ 
+		q: "Which planet lies between Saturn and Neptune?", 
+		a1: "Uranus", 
+		a2: "Jupiter", 
+		a3: "Earth", 
+		a4: "Mars", 
+		corr: 1, 
+		img : "uranus.jpg"
+	},
+	{ 
+		q: "How may moons does the planet Mars have?", 
+		a1: "1", 
+		a2: "2", 
+		a3: "0", 
+		a4: "3", 
+		corr: 2, 
+		img : "mars-phobos_deimos.jpg"
+	},
+	{ 
+		q: "How many stars make up Orion’s Belt?", 
+		a1: "7", 
+		a2: "4", 
+		a3: "5", 
+		a4: "3", 
+		corr: 4, 
+		img : "orion.jpg"
+	},
+	{ 
+		q: "Io, Europa, Ganymede and Callisto are all moons of which planet in our solar system?", 
+		a1: "Saturn", 
+		a2: "Venus", 
+		a3: "Uranus", 
+		a4: "Jupiter", 
+		corr: 4, 
+		img : "jupiter.jpg"
+	},
+	{ 
+		q: "Which is the only planet in our solar system to rotate clockwise?", 
+		a1: "Saturn", 
+		a2: "Mars", 
+		a3: "Mercury", 
+		a4: "Venus", 
+		corr: 4, 
+		img : "venus.jpg"
+	},
+	{ 
+		q: "What is the name of the galaxy that contains the Earth?", 
+		a1: "Milky Way", 
+		a2: "Andromeda", 
+		a3: "Whirpool Galaxy", 
+		a4: "Pinwheel Galaxy", 
+		corr: 1, 
+		img : "milky_way.jpg"
+	},
+	{ 
+		q: "Which is the largest planet in our solar system?", 
+		a1: "Venus", 
+		a2: "Jupiter", 
+		a3: "Saturn", 
+		a4: "Uranus", 
+		corr: 2, 
+		img : "jupiter.jpg"
+	},
+	{ 
+		q: "What is called a star showing a sudden large increase in brightness, then gradually returning to its original state over a period of weeks to years?", 
+		a1: "Nebula", 
+		a2: "Nova", 
+		a3: "Black hole", 
+		a4: "Guest star", 
+		corr: 2, 
+		img : "nova.jpg"
+	},
+	{ 
+		q: "Which planet in our solar system takes the shortest time to orbit the Sun?", 
+		a1: "Venus", 
+		a2: "Earth", 
+		a3: "Mercury", 
+		a4: "Mars", 
+		corr: 3, 
+		img : "mercury.jpg"
+	},
+	{ 
+		q: "Terra is the Latin name for which planet in our solar system?", 
+		a1: "Earth", 
+		a2: "Jupiter", 
+		a3: "Uranus", 
+		a4: "Venus", 
+		corr: 1, 
+		img : "earth.jpg"
+	},
+	{ 
+		q: "Which planet is named after the Roman god of war?", 
+		a1: "Saturn", 
+		a2: "Jupiter", 
+		a3: "Uranus", 
+		a4: "Mars", 
+		corr: 4, 
+		img : "mars.jpg"
+	},
+	{ 
+		q: "Which planet spins the slowest?", 
+		a1: "Mercury", 
+		a2: "Jupiter", 
+		a3: "Venus", 
+		a4: "Mars", 
+		corr: 3, 
+		img : "venus.jpg"
+	},
+	{ 
+		q: "How long does it take light from the Sun to reach the earth?", 
+		a1: "about 8 minutes and 18 seconds", 
+		a2: "about 3 minutes and 30 seconds", 
+		a3: "about 30 minutes", 
+		a4: "about 12 hours", 
+		corr: 1, 
+		img : "sunbeam.jpg"
+	},
+	{ 
+		q: "Which is the largest moon in the solar system?", 
+		a1: "Europa", 
+		a2: "Ganymede", 
+		a3: "Deimos", 
+		a4: "Titania", 
+		corr: 2, 
+		img : "jupiter-moons.gif"
+	},
+	{ 
+		q: "From which areas of space can there be no escape?", 
+		a1: "White holes", 
+		a2: "Whormholes", 
+		a3: "Dubm holes", 
+		a4: "Black holes", 
+		corr: 4, 
+		img : "black_hole.jpg"
+	},
+	{ 
+		q: "What is the name of the thousands of small bodies which orbit the Sun?", 
+		a1: "Comets", 
+		a2: "Asteroids", 
+		a3: "Planets", 
+		a4: "Stars", 
+		corr: 2, 
+		img : "asteroids.jpg"
+	},
+	{ 
+		q: "What can contract to give birth to a star?", 
+		a1: "Nebula", 
+		a2: "Supernova", 
+		a3: "Nova", 
+		a4: "Big bang", 
+		corr: 1, 
+		img : "nebula.jpg"
+	},
+	{ 
+		q: "Proteus and Nereid are moons of which planet?", 
+		a1: "Mars", 
+		a2: "Venus", 
+		a3: "Neptune", 
+		a4: "Uranus", 
+		corr: 3, 
+		img : "neptune.png"
+	},
+	{ 
+		q: "What name is given to the explosive death of a star?", 
+		a1: "Nova", 
+		a2: "Supernova", 
+		a3: "Nebula", 
+		a4: "Hypernova", 
+		corr: 2, 
+		img : "supernova.jpg"
+	},
+	{ 
+		q: "Which is the second largest planet in the solar system?", 
+		a1: "Jupiter", 
+		a2: "Neptune", 
+		a3: "Saturn", 
+		a4: "Uranus", 
+		corr: 3, 
+		img : "saturn.jpg"
+	},
+	{ 
+		q: "Which planet has been the focus of investigations for signs of life?", 
+		a1: "Mars", 
+		a2: "Venus", 
+		a3: "Jupiter", 
+		a4: "Uranus", 
+		corr: 1, 
+		img : "mars.jpg"
+	}
 ];
 
 
@@ -65,6 +287,7 @@ var game = {
 
 	nCorrect : 0,
 	nWrong : 0,
+	unAnswered : 0,
 
 	start : function(){
 
@@ -92,19 +315,17 @@ var game = {
 		
 		this.stopTimer();
 
-		var unAnswered = MaxQuestionsToAsk - (this.nCorrect+this.nWrong);
-
 		var firstRowTab =  $("table").children().eq(1).children("tr").eq(0).children("td");
-    	firstRowTab.eq(0).text("Correct Answers:");
+    	firstRowTab.eq(0).text("Correct Answers");
     	firstRowTab.eq(1).text(this.nCorrect);
 
 		var secondRowTab =  $("table").children().eq(1).children("tr").eq(1).children("td");
-    	secondRowTab.eq(0).text("Incorrect Answers:");
+    	secondRowTab.eq(0).text("Incorrect Answers");
     	secondRowTab.eq(1).text(this.nWrong);
 
 		var thirdRowTab =  $("table").children().eq(1).children("tr").eq(2).children("td");
-    	thirdRowTab.eq(0).text("Unanswered:");
-    	thirdRowTab.eq(1).text(unAnswered);
+    	thirdRowTab.eq(0).text("Unanswered");
+    	thirdRowTab.eq(1).text(this.unAnswered);
 
 		$("#start").hide();
 		$("#timer").hide();
@@ -117,8 +338,10 @@ var game = {
 	askQuestion : function(){
 
     	game.currQuestion++;
+    	var maxQ = MaxQuestionsToAsk;
+    	if(MaxQuestionsToAsk > questionList.length) maxQ = questionList.length;
 
-    	if( game.currQuestion < MaxQuestionsToAsk ){
+    	if( game.currQuestion < maxQ){
 
 			$("#timer").text("Remaining Time: "+ MaxTimer + " Seconds");
     		game.startTimer();
@@ -155,19 +378,36 @@ var game = {
 		var qObj = questionList[this.currQuestion];
 		var answerText = "";
 
-		if(nA[1] == qObj.corr){
+		var correctN = qObj.corr;
+
+		if(nA[1] == correctN){
 			answerText += "Correct!";
 			this.nCorrect++;
 		}
 		else if(nA[1] == 0){
 			answerText += "Out of Time!";
+			this.unAnswered++;
 		}
 		else{
 			answerText += "Incorrect!";
 			this.nWrong++;
 		}
 
-		$("#correct_answer").text(answerText);
+		$("#correct_answer").empty();
+		$("#correct_answer").append( $("<p>").text(answerText) );
+		if(nA[1] != correctN){
+			$("#correct_answer").append( $("<p>").text(
+				"Correct answer: " + qObj["a"+correctN]) );
+		}
+		else{
+			$("#correct_answer").append( $("<p>").text(
+				"It's " + qObj["a"+correctN]) );
+		}
+		var answerImage = $("<img>");
+		answerImage.attr("src", "assets/images/"+qObj.img);
+		answerImage.attr("class", "a_image");
+		// console.log(imgSrc);
+		$("#correct_answer").append(answerImage);
 
 		$("#answer_area").show();
 		$("#question_area").hide();
